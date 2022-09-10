5226114143544a99ad35932838aab6ca1f9d5aa4
myles-ux <fistonrwanda@gmail.com>
2022-09-10 07:42:31 +0200

    mock convers and styled

---
+++ src/__mocks__/conversations.ts
@@ -0,0 +1,99 @@
+import { ConversationType } from "../utils/types";
+
+export default [
+  {
+    id: 1,
+    name: "John Smith",
+    lastMessage: "How are you?",
+  },
+  {
+    id: 2,
+    name: "Hannah Michael",
+    lastMessage: "See you!",
+  },
+  {
+    id: 3,
+    name: " Billy Bob Jones",
+    lastMessage: "I am working right now",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+  {
+    id: 4,
+    name: "Connor McGregor",
+    lastMessage: "Is everything good?",
+  },
+  {
+    id: 5,
+    name: "William Scott",
+    lastMessage: "The best way to work is",
+  },
+] as ConversationType[];
